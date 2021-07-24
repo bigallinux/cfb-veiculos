7e50a1fb9bc75ad08d137ff0b9d8616ce867a0df
fbigal <bigallinux@gmail.com>
2021-07-24 13:17:10 -0300

    aula02
    menu e logo

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <link href="css/estilos.css" rel="stylesheet" type="text/css"/>
+    <title>CFB Veiculos</title>
+  </head>
+  <body>
+      <header>
+          <?php
+            include_once 'topo.php';
+          ?>
+      </header>
+    <main></main>
+    <footer></footer>
+  </body>
+</html>
---
+++ topo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<section id="logo_topo">
+    <img src="imgs/logo.png" />
+</section>
+<nav id="menu">
+    <a class="btmenu" href="index.php" target="_self">Home</a>
+    <a class="btmenu" href="index.php" target="_self">Carros</a>
+    <a class="btmenu" href="index.php" target="_self">Contatos</a>
+    <a class="btmenu" href="index.php" target="_self">Localização</a>
+
+</nav>
