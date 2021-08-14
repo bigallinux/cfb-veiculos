2f726fdd50adc92b242c20d621df5e9349b3e3f4
fbigal <bigallinux@gmail.com>
2021-08-14 19:28:13 -0300

    aula 4
    buscador

---
+++ buscador.php
@@ -0,0 +1,34 @@
+<p class="titulo">Encontre um veiculo</p>
+<form name="fbuscador" id="fbuscador" action="#" method="post">
+    <div class="div_busca">
+        <label >Selecione a marca</label>
+        
+        <select name="marca" id="id_marcas">
+            <option value=""></option>
+
+        </select>
+        
+    </div>
+    <div class="div_busca">
+        <label>Selecione a modelo</label>
+        
+        <select name="modelo" id="id_modelos">
+            <option value=""></option>
+
+        </select>
+        
+    </div>
+    <div class="div_busca">
+        <input type="submit" value="Buscar" name="f_submit" class="btmenu">
+    </div>
+</form>
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="">
+<html lang="pt-br">
   <head>
     <meta charset="utf-8">
     <link href="css/estilos.css" rel="stylesheet" type="text/css"/>
@@ -11,12 +11,19 @@
             include_once 'topo.php';
           ?>
       </header>
+      
       <section id="slider">
           <?php
                     include_once 'slider.html';
           ?>
       </section>
-    <main></main>
+      
+      <section id="buscador">
+          <?php
+                    include_once 'buscador.php';
+          ?>
+      </section>
+      <main></main>
     <footer></footer>
   </body>
 </html>
