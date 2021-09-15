5810951471c053ad58353b751f0f17cd4047d82f
fbigal <bigallinux@gmail.com>
2021-09-15 16:13:44 -0300

    aula 16
    Rotina MySQL para excluir um colaborador

--- excluir_usuario.php
+++ excluir_usuario.php
@@ -36,14 +36,30 @@
           <?php
             if(isset($_GET["f_bt_excluir_colaborador"])){
                 $vid=$_GET["f_colaboradores"];
-            }
+                $query = "delete from tb_colaboradores where id_colaborador=$vid";
+                mysqli_query($con, $query);
+                $linhas= mysqli_affected_rows($con);
+                if($linhas>=1){
+                    echo'<p>Colaborador deletado com sucesso</p>';
+                }else{
+                    echo '<p>Erro ao deletar colaborador</p>';
+                };
+            };
           ?>
           
           <form name="f_excluir_colaborador" action="excluir_usuario.php" class="f_colaborador" method="get">
               <input type="hidden" name="num" value="<?php echo $n1; ?>">
               <label>Selecione o colaborador</label>
               <select name="f_colaboradores" size="10">
-                  <option value="id_col">nome_col</option>
+                  <?php
+                  $query = "select * from tb_colaboradores;";
+                  $col = mysqli_query($con, $query);
+                  $total_col = mysqli_num_rows($col);
+                  
+                  while ($exibe= mysqli_fetch_array($col)){
+                      echo'<option value="'.$exibe["id_colaborador"].'">'.$exibe["nome"].'</option>';
+                  };
+                  ?>
               </select>
               <input type="submit" name="f_bt_excluir_colaborador" class="btmenu" value="excluir">
                  
