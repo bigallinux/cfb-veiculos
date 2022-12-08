cac566a17f9e7e5a9bbd03bf47c5792b3d048ae4
fbigal <bigallinux@gmail.com>
2022-12-08 19:24:06 -0300

    aula 21
    PSC - Aula 21 - SQL para deletar Marcas e Modelos

--- marcas_modelos.php
+++ marcas_modelos.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
-if(isset($_SESSION["numlogin"])){
-$n1 = $_GET["num"];
-$n2 = $_SESSION["numlogin"];
-if($n1 != $n2){
-echo '<p>Login não efetuado</p>';
-exit;
-}
-}else{
-echo '<p>Login não efetuado</p>';
-exit;
+if (isset($_SESSION["numlogin"])) {
+    $n1 = $_GET["num"];
+    $n2 = $_SESSION["numlogin"];
+    if ($n1 != $n2) {
+        echo '<p>Login não efetuado</p>';
+        exit;
+    }
+} else {
+    echo '<p>Login não efetuado</p>';
+    exit;
 }
 include 'conexao.inc';
 ?>
@@ -33,39 +33,55 @@ include 'conexao.inc';
             <h1>Marcas / Modelos</h1>
 
             <?php
-            if(isset($_GET["codigo"])){
-            $vcod = $_GET["codigo"];
-            if($vcod==1){
-            //nova marca
-            $vmarca = $_GET["f_marca"];
-            $sql = "INSERT INTO tb_marcas (marca) values ('$vmarca')";
-            mysqli_query($con, $sql);
-            $linha = mysqli_affected_rows($con);
-            if ($linhas >= 1){
-            echo"<script>alert('Nova marca adicionada.');</script>";
-
-            }else {
-            echo"<script>alert('ERRO! Marca não adicionada.');</script>";
-            }
-            }elseif ($cod==2) {
-            //novo modelo
-            $vmodelo = $_GET["f_modelos"];
-            $vidmarca = $_GET["f_marcas"];
-            $sql = "INSERT INTO tb_modelos (modelo,id_marca) values ('$vmodelo',$vidmarca)";
-            mysqli_query($con, $sql);
-            $linha = mysqli_affected_rows($con);
-            if ($linhas >= 1){
-            echo"<script>alert('Novo modelo adicionado.');</script>";
-
-            }else {
-            echo"<script>alert('ERRO! Modelo não adicionada.');</script>";
-            }
-            }elseif ($cod==3) {
-            //deleta marcar
-            }elseif ($cod==4) {
-            //deleta modelo
-            }
-            }
+            if (isset($_GET["codigo"])) {
+                $vcod = $_GET["codigo"];
+                if ($vcod == 1) {
+                    //nova marca
+                    $vmarca = $_GET["f_marca"];
+                    $sql = "INSERT INTO tb_marcas (marca) values ('$vmarca');";
+                    mysqli_query($con, $sql);
+                    $linha = mysqli_affected_rows($con);
+                    if ($linha >= 1) {
+                        echo"<script>alert('Nova marca adicionada.');</script>";
+                    } else {
+                        echo"<script>alert('ERRO! Marca não adicionada.');</script>";
+                    }
+                } elseif ($vcod == 2) {
+                    //novo modelo
+                    $vmodelo = $_GET["f_modelo"];
+                    $vidmarca = $_GET["f_id_marca"];
+                    $sql = "INSERT INTO tb_modelos (modelo,id_marca) values ('$vmodelo',$vidmarca);";
+                    mysqli_query($con, $sql);
+                    $linha = mysqli_affected_rows($con);
+                    if ($linha >= 1) {
+                        echo"<script>alert('Novo modelo adicionado.');</script>";
+                    } else {
+                        echo"<script>alert('ERRO! Modelo não adicionada.');</script>";
+                    }
+                } elseif ($vcod == 3) {
+                    //deleta marcar
+                    $vidmarca = $_GET["f_id_marca"];
+                    $sql = "delete from tb_marcas where id_marca = $vidmarca; ";
+                    mysqli_query($con, $sql);
+                    $linha = mysqli_affected_rows($con);
+                    if ($linha >= 1) {
+                        echo"<script>alert('Deletada marca .');</script>";
+                    } else {
+                        echo"<script>alert('ERRO! Marca não deletada.');</script>";
+                    }
+                } elseif ($vcod == 4) {
+                    //deleta modelo
+                    $vidmodelo = $_GET["f_id_modelo"];
+                    $vidmarca = $_GET["f_id_marcas"];
+                    $sql = "delete from tb_modelos where id_modelo = $vidmodelo and id_marca = $vidmarca;";
+                    mysqli_query($con, $sql);
+                    $linha = mysqli_affected_rows($con);
+                    if ($linha >= 1) {
+                        echo"<script>alert('Deletado modelo .');</script>";
+                    } else {
+                        echo"<script>alert('ERRO! Modelo não deletado.');</script>";
+                    }
+                }
             }
             ?>
 
@@ -84,11 +100,11 @@ include 'conexao.inc';
                     <input type="hidden" name="num" value="<?php echo $n1; ?>">
                     <input type="hidden" name="codigo" value="2">
                     <label>Selecione um marca</label>
-                    <select name="f_marcas" size="10" required="required">
+                    <select name="f_id_marca" size="10" required="required">
                         <?php
                         $sql = "Select * from tb_marcas;";
                         $col = mysqli_query($con, $sql);
-                        //$total_col= mysqli_num_rows($col);
+//$total_col= mysqli_num_rows($col);
                         while ($exibe = mysqli_fetch_array($col)) {
                             echo "<option value='" . $exibe['id_marca'] . "'>" . $exibe['marca'] . "</option>";
                         }
@@ -107,11 +123,11 @@ include 'conexao.inc';
                     <input type="hidden" name="num" value="<?php echo $n1; ?>">
                     <input type="hidden" name="codigo" value="3">
                     <label>Selecione um marca</label>
-                    <select name="f_marcas" size="10" required="required">
+                    <select name="f_id_marca" size="10" required="required">
                         <?php
                         $sql = "Select * from tb_marcas;";
                         $col = mysqli_query($con, $sql);
-                        //$total_col= mysqli_num_rows($col);
+//$total_col= mysqli_num_rows($col);
                         while ($exibe = mysqli_fetch_array($col)) {
                             echo "<option value='" . $exibe['id_marca'] . "'>" . $exibe['marca'] . "</option>";
                         }
@@ -125,11 +141,11 @@ include 'conexao.inc';
                     <input type="hidden" name="num" value="<?php echo $n1; ?>">
                     <input type="hidden" name="codigo" value="4">
                     <label>Selecione um modelo</label>
-                    <select name="f_modelo" size="10" required="required">
+                    <select name="f_id_modelo" size="10" required="required">
                         <?php
                         $sql = "Select * from tb_modelos;";
                         $col = mysqli_query($con, $sql);
-                        //$total_col= mysqli_num_rows($col);
+//$total_col= mysqli_num_rows($col);
                         while ($exibe = mysqli_fetch_array($col)) {
                             echo "<option value='" . $exibe['id_modelo'] . "'>" . $exibe['modelo'] . "</option>";
                         }
